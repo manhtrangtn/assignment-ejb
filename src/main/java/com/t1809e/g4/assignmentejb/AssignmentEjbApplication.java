a8bfeaec427dce1cde4a1b7b717382c98e344425
phuc <phucquan0106@gmail.com>
2020-09-21 20:59:06 +0700

    a

--- src/main/java/com/t1809e/g4/assignmentejb/AssignmentEjbApplication.java
+++ src/main/java/com/t1809e/g4/assignmentejb/AssignmentEjbApplication.java
@@ -2,8 +2,10 @@ package com.t1809e.g4.assignmentejb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.t1809e.g4.assignmentejb.*.*")
 public class AssignmentEjbApplication {
 
 	public static void main(String[] args) {
