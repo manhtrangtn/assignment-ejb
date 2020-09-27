4752c818436fcb659ef176803e399352b3aca8f3
manhtrangtn <manhtrangtn@gmail.com>
2020-09-28 00:27:02 +0700

    Fix some bugs

--- src/main/java/com/t1809e/g4/assignmentejb/repository/UserRepository.java
+++ src/main/java/com/t1809e/g4/assignmentejb/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.t1809e.g4.assignmentejb.repository;
 
 import com.t1809e.g4.assignmentejb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
     @Query("select u from User u where u.firstName like :keyword or u.lastName like :keyword")
     List<User> findAllByFirstNameOrLastNameLike(@Param("keyword") String keyword);
 }
